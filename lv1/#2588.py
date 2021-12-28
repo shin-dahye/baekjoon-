6f6fa41ee1ef08572555641a38c79bd4a1ab01bf
shin-dahye <91268385+shin-dahye@users.noreply.github.com>
2021-12-28 13:08:34 +0900

    Update #2588.py

--- lv1/#2588.py
+++ lv1/#2588.py
@@ -7,4 +7,14 @@ print(a * (b//100))
 print(a * b)
 
 #print 여러번 쓰지 않기
+
 print(a*(b%10),a*((b%100)//10),a*(b//100),a*b,sep='\n')
+
+#range 사용해서 역순으로
+
+num1 = int(input())
+num2 = input()
+
+for i in range(len(num2), 0, -1):
+    print(num1 * int(num2[i-1]))
+print(num1 * int(num2))
